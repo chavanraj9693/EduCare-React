970e7892724d215817dd66a39e3e43b99fe28196
Raj Chavan <91159019+chavanraj9693@users.noreply.github.com>
2023-06-11 20:24:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/
-EduCare-React
->
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
